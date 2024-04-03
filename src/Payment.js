c92246bbfdc9023f8df50b98bbc2cb210c4544f8
MohammedImran03 <mohammedimran1407.mi@gmail.com>
2024-04-03 22:08:53 +0530

    client

--- src/Payment.js
+++ src/Payment.js
@@ -5,14 +5,23 @@ import { useParams } from 'react-router-dom';
 
 const Payment = () => {
 
-    const { data } = useParams();
+  const queryParams = new URLSearchParams(window.location.search);
+
+  // Get the individual query parameters
+  const userId = queryParams.get('user_id');
+  const amount = queryParams.get('amount');
+  const productId = queryParams.get('product_id');
 
 
   return (
 <div className='flex flex-col justify-center items-center h-full centerdiv'>
   <div className='text-center text-xl text-white font-semibold'>Tune Tutor</div>
   <div className='text-center text-md my-2 text-white font-semibold'>Enroll Your Course Now</div>
-  <div className='text-center text-md my-2 text-white font-semibold'>{data}</div>
+  
+  <div className='text-center text-md my-2 text-white font-semibold'>
+        User ID: {userId}, Amount: {amount}, Product ID: {productId}
+      </div>
+
 <button className='cursor-pointer text-white bg-blue-600 rounded-md px-4 py-2 mt-2 text-md font-semibold'>Buy Course</button>
 </div>
 
